5e7443a81c26fcfe2a4e1ae95258cbb40fa4d254
whomiri <75207175+whomiri@users.noreply.github.com>
2020-12-13 21:07:04 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,8 +4,8 @@ import logging, random, string, time
 import threading 
 import os
 
-TOKEN= ['1408034311:AAFW5_6PsLCUbJ2H2wcGv-xDAg8oru-8Zkc']
-bot_id = TOKEN= ['1408034311']
+TOKEN= '1408034311:AAFW5_6PsLCUbJ2H2wcGv-xDAg8oru-8Zkc'
+bot_id = int(TOKEN.split(':')[0])
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
